34923fb99605b905ce0ef210c08d77bec532f930
绚丽彩虹 <flandrestudio.cn@gmail.com>
2020-09-08 03:08:13 +0800

    1

--- assets/moeplayer/js/moeplayer.js
+++ assets/moeplayer/js/moeplayer.js
@@ -1,59 +1,93 @@
 var moeplayer = {
-	config:{},
-	audio:null,
-	video:null,
+	config: {},
+	audio: null,
+	video: null,
 	
-	/**
-	 * 设置配置信息
-	 * 
-	 * @param object config 配置信息
-	 * @return null
-	 */
-	setConfig(config){
+	helper:{
+		makeUUID(){
+			
+		}
+	},
+	
+	init(config){
 		this.config = config;
+		this.audio = new Auido;
+		this.video = document.createElement("VIDEO");
 	},
 	
-	/**
-	 * 初始化
-	 * 
-	 * @return null
-	 */
-	init(){
-		this.audio = new Audio();
-		this.video = document.createElement('VIDEO')
-	},
-	
-	//歌单部分
 	list:{
-		//设置
-		setList(){
+		store:{},
+		
+		add(listInfo){
 			
 		},
-		//添加歌单
-		addList(){
+		
+		remove(){
 			
 		},
-		//添加歌单内歌曲
+		
 		addMusic(){
 			
 		},
-		//删除歌单
-		deleteList(){
-			
-		}
-		//删除歌单内歌曲
+		
 		deleteMusic(){
 			
 		}
+	},
+	
+	controller:{
+		nowElement:null,
+		
+		play(){
+			
+		},
+		
+		pause(){
+			
+		},
+		
+		setMusic(){
+			
+		},
+		
+		setPlayType(){
+			
+		},
+		
+		next(){
+			
+		},
+		
+		last(){
+			
+		},
+		
+		muted(){
+			
+		},
+		
+		volume(){
+			
+		},
+		
+		time(){
+			
+		},
+		
+		src(){
+			
+		},
+		
+		playbackRate(){
+			
+		}
+	},
+	
+	assetsParser:{
+		parsers:{},
+		
+		add(){
+			
+		}
 	}
-	
-	//播放控制
-		//播放
-		//暂停
-		//获取当前播放信息
-		//
-	
-	
-	//事件监听注册
-	
 };
